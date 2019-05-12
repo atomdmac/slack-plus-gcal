f0e3045183ee4152e09f8f74a932d27727a961ac
Atom Mac <atom.mac@gmail.com>
2019-05-12 17:42:41 -0400

    Initial Commit!

---
+++ src/db/index.js
@@ -0,0 +1,43 @@
+const Knex = require('knex');
+const { Model } = require('objection');
+
+const {
+  POSTGRES_HOST: host,
+  POSTGRES_USER: user,
+  POSTGRES_PASSWORD: password,
+  POSTGRES_DATABASE: database
+} = process.env;
+
+const knex = Knex({
+  client: 'pg',
+  useNullAsDefault: false,
+  connection: {
+    host,
+    user,
+    password,
+    database
+  }
+})
+
+Model.knex(knex);
+
+class User extends Model {
+  static get tableName() {
+    return 'User'
+  }
+
+  async function createSchema() {
+    if (await knex.schema.hasTable(User.tableName)) {
+      return;
+    }
+
+    await knex.schema.createTable(User.tableName, callback)
+  }
+}
+
+module.exports = {
+  knex,
+  models: {
+    User
+  }
+};
